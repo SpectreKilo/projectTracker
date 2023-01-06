1c47dd2822ebeb8adde38ede2fe59015e7c07089
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-01-06 12:38:56 -0800

    finish modal form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var trackerEl = $('#tracker-section');
 var projectFormButton = $('#project-form');
 
 
+
 var currentDate = dayjs().format("MMM D, YYYY h:mm:ss");
 
 $("#headerDT").text(currentDate);
@@ -10,6 +11,28 @@ $("#headerDT").text(currentDate);
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
+projectFormButton.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+
+
+
+// Autocomplete widget
+$(function () {
+    var ProjectTypes = [
+        'front end',
+        'back end',
+        'mobile'
+    ];
+    $('#project-type').autocomplete({
+      source: ProjectTypes,
+    });
+  });
+
+// Datepicker widget
+$(function () {
+    $('#datepicker').datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  });
