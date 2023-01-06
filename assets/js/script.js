d18ef30862d5203e078ffb9683b9ebd99bc42f86
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-01-06 12:25:24 -0800

    added card and modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-var currentDate = dayjs().format("MMM D, YYYY");
-console.log(dayjs())
+var sideBarEl = $('#side-bar');
+var trackerEl = $('#tracker-section');
+var projectFormButton = $('#project-form');
+
+
+var currentDate = dayjs().format("MMM D, YYYY h:mm:ss");
+
 $("#headerDT").text(currentDate);
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
